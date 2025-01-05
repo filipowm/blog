accd64d1427b81652132c6d2b6559f7aa9d112cb
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2025-01-05 23:54:55 +0100

    f

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '',
+  title: 'Mateusz Filipowicz',
   subtitle: 'My Tech, Hobby and Travel blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
